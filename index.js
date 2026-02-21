591cb9fd8136586459e0c71cb251e8d9d448dcdd
HoneySecret20 <150463386+HoneySecret20@users.noreply.github.com>
2026-02-21 18:27:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
 
+app.use(express.json({
+  verify: (req, res, buf) => {
+    req.rawBody = buf;
+  }
+}));
 // Routes
 app.use("/api/auth", authRoutes);
 
